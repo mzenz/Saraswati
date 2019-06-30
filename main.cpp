eeb4ed4eee1431519b06d8c29dad1c15a6f923aa
Martin Z <mzenze@gmail.com>
2019-06-30 10:49:19 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+// Project: midi_to_freq
+//
+// Created with cpp_boot.py
+// Martin Zenzerovich
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	cout << "Hello world!" << endl;
+}
