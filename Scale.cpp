ed477755be28fed2b33c3e651136b13a91bfedbe
Martin Z <mzenze@gmail.com>
2019-07-01 23:52:33 -0300

    refactor

---
+++ Scale.cpp
@@ -0,0 +1,25 @@
+#include "Scale.h"
+
+std::vector<Note> majorScale(const Note& note) {
+	std::vector<Note> scale;
+	Note n = note;
+	for (auto i = 0; i < 7; ++i) {
+		scale.push_back(n);
+		
+		// stop iterating if we are at
+		// the highest possible note
+		if (n == Note::max()) {
+			break;
+		}
+
+		switch(i) {
+			case 2:
+			case 6:
+			++n;
+			break;
+		default:
+			n += 2;
+		}
+	}
+	return scale;
+}
---
+++ Scale.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Note.h"
+#include <vector>
+
+/// Returns a list of notes representing the major scale for this note
+std::vector<Note> majorScale(const Note& note);
